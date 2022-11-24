f3905c4c3553e38c3ae69eee00315f6881f03de8
Martin-Turtle <info@martinturtle.co.uk>
2022-11-24 12:35:36 +0000

    Work for Question 6

---
+++ src/ReverseString.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+        System.out.print ("Input a string : ");
+        char[] letters = reader.nextLine().toCharArray();
+
+
+        String originalStr = String.valueOf(letters);
+        String reversedStr = "";
+
+        for (int i = 0; i < originalStr.length(); i++) {
+            reversedStr = originalStr.charAt(i) + reversedStr;
+        }
+
+        System.out.println("Reversed string: "+ reversedStr);
+
+    }
+}
+
